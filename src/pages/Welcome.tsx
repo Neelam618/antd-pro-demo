da2ea2c15eac37fea72b7a6cc6df36e339e50384
Neelam618 <neelamchavan410@gmail.com>
2021-05-11 18:06:49 +0530

    changed welcome page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,63 +1,27 @@
 import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
-import { Card, Alert, Typography } from 'antd';
-import { useIntl, FormattedMessage } from 'umi';
-import styles from './Welcome.less';
-
-const CodePreview: React.FC = ({ children }) => (
-  <pre className={styles.pre}>
-    <code>
-      <Typography.Text copyable>{children}</Typography.Text>
-    </code>
-  </pre>
-);
+import { Row, Col } from 'antd';
 
 export default (): React.ReactNode => {
-  const intl = useIntl();
   return (
     <PageContainer>
-      <Card>
-        <Alert
-          message={intl.formatMessage({
-            id: 'pages.welcome.alertMessage',
-            defaultMessage: '更快更强的重型组件，已经发布。',
-          })}
-          type="success"
-          showIcon
-          banner
-          style={{
-            margin: -12,
-            marginBottom: 24,
-          }}
-        />
-        <Typography.Text strong>
-          <FormattedMessage id="pages.welcome.advancedComponent" defaultMessage="高级表格" />{' '}
-          <a
-            href="https://procomponents.ant.design/components/table"
-            rel="noopener noreferrer"
-            target="__blank"
+       <Row gutter={[48, 48]} justify="center" align="middle">
+        <Col xl={12}>
+          <h1
+            className="text-xl lg:text-5xl font-bold text-gray-800 text-center lg:text-left mb-0"
+            style={{ lineHeight: '50px' }}
           >
-            <FormattedMessage id="pages.welcome.link" defaultMessage="欢迎使用" />
-          </a>
-        </Typography.Text>
-        <CodePreview>yarn add @ant-design/pro-table</CodePreview>
-        <Typography.Text
-          strong
-          style={{
-            marginBottom: 12,
-          }}
-        >
-          <FormattedMessage id="pages.welcome.advancedLayout" defaultMessage="高级布局" />{' '}
-          <a
-            href="https://procomponents.ant.design/components/layout"
-            rel="noopener noreferrer"
-            target="__blank"
-          >
-            <FormattedMessage id="pages.welcome.link" defaultMessage="欢迎使用" />
-          </a>
-        </Typography.Text>
-        <CodePreview>yarn add @ant-design/pro-layout</CodePreview>
-      </Card>
+            headingTxt
+          </h1>
+          <p className="text-xl text-gray-700 text-center lg:text-left mt-0 lg:mt-2">paraTxt</p>
+          <div className="mt-8">
+            <input type="text" />
+          </div>
+        </Col>
+        <Col xl={12}>
+          <img src='https://assets.market-storefront.envato.com/storefront/packs/media/images/home/themeforest/search-header-e9b1f87ec2352b50a7d1b2da92ee268d.png' alt='' />
+        </Col>
+      </Row>
     </PageContainer>
   );
 };
